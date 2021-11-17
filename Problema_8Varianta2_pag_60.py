a07ad99439f22aa8767275af184d3d25acef061d
VCebotarosh <vladimir.cebotarosh@gmail.com>
2021-11-17 20:04:52 +0200

    Second Commit

---
+++ Problema_8Varianta2_pag_60.py
@@ -0,0 +1,26 @@
+multime=input("Dati elementele primei multimi: ")
+multime=list(multime.strip().split(" "))
+multimea_a=set(multime)
+multime=input("Dati elementele multimii a doua: ")
+multime=list(multime.strip().split(" "))
+multime_b=set(multime)
+multimea_a_filter=[]
+multime_b_filter=[]
+for element in multimea_a:
+    try:
+        a=int(element)
+        multimea_a_filter.append(a)
+    except ValueError:
+        continue
+for element in multime_b:
+    try:
+        b=int(element)
+        multime_b_filter.append(b)
+    except ValueError:
+        continue
+multimea_a_filter=set(multimea_a_filter)
+multime_b_filter=set(multime_b_filter)
+print(f"Intersectia celor 2 multimi este : {multimea_a_filter.intersection(multime_b_filter)}")
+print(f"Reuniunea celor 2 multimi este : {multimea_a_filter.union(multime_b_filter)}")
+print(f"Diferenta multimii A {multimea_a_filter} si multimii B {multime_b_filter} este : {multimea_a_filter.difference(multime_b_filter)}")
+print(f"Diferenta multimii B {multime_b_filter} si multimii A {multimea_a_filter} este : {multime_b_filter.difference(multimea_a_filter)}")
--- Problema_8_pag_60.py
+++ Problema_8_pag_60.py
@@ -1,16 +1,11 @@
-multime=input("Dati elementele primei multimi: ")
-multime=list(multime.strip().split(" "))
-multimea_a=set(multime)
-multime=input("Dati elementele multimii a doua: ")
-multime=list(multime.strip().split(" "))
-multime_b=set(multime)
-if all(element.isdigit() for element in multimea_a) and all (element.isdigit()for element in multime_b):
-        print(f"Intersectia celor 2 multimi este : {multimea_a.intersection(multime_b)}")
-        print(f"Reuniunea celor 2 multimi este : {multimea_a.union(multime_b)}")
-        print(f"Diferenta multimii A {multimea_a} si multimii B {multime_b} este : {multimea_a.difference(multime_b)}")
-        print(f"Diferenta multimii B {multime_b} si multimii A {multimea_a} este : {multime_b.difference(multimea_a)}")
-else:
-    print(f"Ceea ce ati introdus nu satisface conditiile problemei")
-
-
-
+try:
+    multime=input("Dati elementele primei multimi: ")
+    multimea_a=set(list(map(lambda x: int(x), multime.strip().split(" "))))
+    multime=input("Dati elementele multimii a doua: ")
+    multimea_b=set(list(map(lambda x: int(x), multime.strip().split(" "))))
+    print(f"Intersectia celor 2 multimi este : {multimea_a.intersection(multimea_b)}")
+    print(f"Reuniunea celor 2 multimi este : {multimea_a.union(multimea_b)}")
+    print(f"Diferenta multimii A {multimea_a} si multimii B {multimea_b} este : {multimea_a.difference(multimea_b)}")
+    print(f"Diferenta multimii B {multimea_b} si multimii A {multimea_a} este : {multimea_b.difference(multimea_a)}")
+except ValueError:
+    print("Datele introduse nu satisfac conditiile problemei")
